cf3dd647496ee835d4fb493d5c0b5187bde59394
CBorrello <164372690+CBorrello@users.noreply.github.com>
2025-03-25 21:31:04 +0100

    Initial Commit

---
+++ HabitTrackerApp/HabitTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppApp.swift
+//  HabitTrackerApp
+//
+//  Created by Christian on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackerAppTests/HabitTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppTests.swift
+//  HabitTrackerAppTests
+//
+//  Created by Christian on 25/03/25.
+//
+
+import Testing
+@testable import HabitTrackerApp
+
+struct HabitTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
